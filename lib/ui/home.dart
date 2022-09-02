16e0b08e4bf5dad6e69602db0670ca2a62c97747
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-09-02 21:42:27 +0600

    Details Route:Adding Horizontal Line & More movie poster.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,23 +6,6 @@ class movieListview extends StatelessWidget {
 
   final List<Movie> movieList = Movie.getMovies();
 
-  final List movies = [
-    "Avatar",
-    "Titanic",
-    "SpiderMan Home Coming",
-    "Thor",
-    "The Avengers",
-    "Doctor Strange",
-    "Minions",
-    "Despicable Me",
-    "Despicable Me 2",
-    "Despicable Me 3",
-    "Ice age",
-    "Ice age 2",
-    "Ice age 3",
-  ];
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -46,35 +29,6 @@ class movieListview extends StatelessWidget {
                     child: movieImage(movieList[index].images[0])),
 
               ]);
-
-
-       /* return Card(
-          elevation: 50,
-          shadowColor: Colors.red,
-              color: Colors.lightGreen,
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                 borderRadius: BorderRadius.circular(13.9),
-                  image: DecorationImage(
-                      image: NetworkImage(movieList[index].images[0]),
-                          fit: BoxFit.cover
-
-                  )
-                ),
-                child: null,
-              ),
-            ),
-            title: Text(movieList[index].title,style: TextStyle(color: Colors.white),),
-            subtitle: Text("${movieList[index].director}",style: TextStyle(color: Colors.grey)),
-            trailing: Text("..."),
-            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context)=> MovieListViewDetails(movieName: movieList[index].title,
-              movie: movieList[index],))),
-          ),
-        );*/
       }),
     );
   }
@@ -184,15 +138,6 @@ class MovieListViewDetails extends StatelessWidget {
             MovieDetailsMorePoster(posters: movie.images)
          ],
        )
-      // Center(
-      //   child: Container(
-      //     child: ElevatedButton(
-      //         onPressed: ()=>{
-      //          Navigator.pop(context)
-      //     },
-      //         child: Text("Back ${this.movie.language}"))
-      //   ),
-      // ),
       
     );
   }
